1f342fe49d81635daf3e2114e49e47279d88c8b3
TobiasFavero <tobiasfavero14@gmail.com>
2022-06-27 22:34:21 +0200

    Changed errors return object key from message to error.

--- src/backend/routes/errors.py
+++ src/backend/routes/errors.py
@@ -11,21 +11,21 @@ error_bp = Blueprint("errors", __name__)
 @error_bp.app_errorhandler(NotFound)
 def handle_not_found(error):
     print(traceback.format_exc())
-    return { "message": "This resource is not available" }, 404
+    return { "error": "This resource is not available" }, 404
 
 
 @error_bp.app_errorhandler(ValidationError)
 def handle_invalid_data(error):
     print(traceback.format_exc())
-    return { "message": error.messages }, 400
+    return { "error": error.messages }, 400
 
 @error_bp.app_errorhandler(DatabaseError)
 def handle_invalid_data(error):
     print(traceback.format_exc())
-    return { "message": error.message }, 500
+    return { "error": error.message }, 500
 
 
 @error_bp.app_errorhandler(Exception)
 def handle_generic_exception(error):
     print(traceback.format_exc())
-    return { "message": "Unknown error occured" }, 500
+    return { "error": "Unknown error occured" }, 500
