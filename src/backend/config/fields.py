f8e99ba8d61dac14d2f829295211d8bfaf5262d4
TobiasFavero <tobiasfavero14@gmail.com>
2022-06-27 18:49:03 +0200

    Removed default img url, img url is now required.

--- src/backend/config/fields.py
+++ src/backend/config/fields.py
@@ -7,5 +7,4 @@ class PhoneCaseFieldsValidValues:
 class ProductFieldsValidValues:
     def __init__(self):
         self.product_types = ("case",)
-        self.default_img_url = "https://media.istockphoto.com/photos/multicolor-mobile-phone-plastic-cases-picture-id487000910?k=20&m=487000910&s=612x612&w=0&h=F3DVzb89tPiyHHn7c_iX11UUcHbdOUactu7yW0SFLHI="
 
